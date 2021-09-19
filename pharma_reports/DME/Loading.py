8b69bec23dbc067bd516fba9fca69499a3e57418
Charlie <charlie@macuject.com>
2021-09-19 10:52:01 +1000

    Wrote Loading.py class

--- pharma_reports/DME/Loading.py
+++ pharma_reports/DME/Loading.py
@@ -1,4 +1,5 @@
 from DataSeparator import *
+import matplotlib.pyplot as plt
 
 class Loading(DataSeparator):
 
@@ -23,6 +24,22 @@ class Loading(DataSeparator):
         comb_df = comb_df.sort_values('count', ascending=False)
         return comb_df
 
+    def plot_combinations(self):
+        plt.rcdefaults()
+        colrs = ['#03254c', '#1167b1', '#187bcd', '#2a9df4', '#d0efff']
+        fig, ax = plt.subplots(figsize=(12, 8))
+        comb_df = self.interval_combinations()
+        combs = comb_df['comb']
+        y_pos = np.arange(len(combs))
+        interval_length = comb_df['count']
+        ax.barh(y_pos, interval_length, align='center', color=colrs)
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(combs)
+        ax.invert_yaxis()  # labels read top-to-bottom
+        ax.set_xlabel('Interval Pattern: Number of Occurrences', fontsize=18)
+        plt.show()
+
+
 if __name__ == "__main__":
-    obj = Loading('all', '/devchau.csv')
+    obj = Loading('all', '/ericmayer.csv')
     print(obj.interval_combinations())
